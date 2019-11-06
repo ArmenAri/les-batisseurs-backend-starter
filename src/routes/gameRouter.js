b859f914de1392a127fa5a591f2555fd6f64972f
Armen Aristakesyan <armenaristakes@gmail.com>
2019-11-06 08:10:22 +0100

    :fire: Changing port

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -4,10 +4,12 @@ const router = express.Router()
 router.post("/", function(req, res) {
   const name = req.body.name
   const numberOfPlayers = req.body.numberOfPlayers
+  const shuffle = req.body.shuffle || true
 
   res.send({
-    name: name,
-    numberOfPlayers: numberOfPlayers
+    name,
+    numberOfPlayers,
+    shuffle
   })
 })
 
