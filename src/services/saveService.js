e2de6af8f6e4192881e007722f1da70c461b10d9
Alexis Georges <alexOL98@hotmail.fr>
2019-11-06 09:53:20 +0100

    Debugging saveGame

--- src/services/saveService.js
+++ src/services/saveService.js
@@ -1,16 +1,15 @@
-import writeFile from "../utlis/writeFile"
+import writeFile from "../utils/writeFile"
 import readFile from "../utils/readFile"
 
 export async function saveGame(game) {
-  var gameObj = JSON.parse(game)
-  var gameContent = JSON.stringify(gameObj)
+  var gameContent = JSON.stringify(game)
   var PATHNAME = "../storage/" + game.id + ".json"
 
   await writeFile(PATHNAME, gameContent)
 }
 
 export async function loadGame(gameId) {
-  const gameData = await readFile("../storage/" + gameId + ".json")
-  var jsonParsed = JSON.parse(gameData)
-  return jsonParsed
+  const gameContent = await readFile("../storage/" + gameId + ".json")
+  var game = JSON.parse(gameContent)
+  return game
 }
